538c7b870614f44c54047bcf68533b94ac887d56
Mark Mackenzie <snapdragonxc@gmail.com>
2018-10-02 10:52:07 +1000

    disabled redux logger

--- src/app.jsx
+++ src/app.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import reducers from './reducers';
 import Main from './main';
 
-const store = createStore(reducers, applyMiddleware(thunk, logger));
+// const store = createStore(reducers, applyMiddleware(thunk, logger));
+const store = createStore(reducers, applyMiddleware(thunk));
 ReactDOM.render(
   <Provider store={ store }>
     <Main />
